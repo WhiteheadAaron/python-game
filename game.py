29ea6b87ad61b3ad772d774389daa6034d61f3cd
Aaron Whitehead <whitehead.aaron@live.com>
2018-11-22 18:29:31 -0700

    progress with the game

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import random
 import time
 
+
 def intro():
     print()
     print("It is the zombie apocolypse.")
@@ -12,8 +13,15 @@ def intro():
     while ready != "1":
         ready = input("Are you ready? Enter 1 to continue. ")
     print()
-    print("First game choice")
+    print("You are at your family house with your parents, teenage brother, and younger sister.")
+    time.sleep(2)
     print()
+    print("Your dad wants to go to the store to get supplies. Your mom thinks you should stay put.")
+    time.sleep(2)
+    print()
+    print("Choice 1 is going to the store, choice 2 is waiting at home and barricading the doors")
+    print()
+
 
 def choosePath():
     path = ""
@@ -21,26 +29,139 @@ def choosePath():
         path = input("Which path will you choose? (1 or 2): ")
     return path
 
-def checkPath(chosenPath):
-    print()
-    print("Something happened")
-    time.sleep(2)
-    print()
-    print("something else")
-    time.sleep(2)
-    print()
-    correctPath = random.randint(1, 2)
-    if chosenPath == str(correctPath):
-        print("something good happens")
-        print()
+
+path1 = ""
+
+
+def chooseDirection(chosenPath):
+    if chosenPath == "1":
+        return storePath()
     else:
-        print("Something bad happens")
+        return stayHomePath()
+
+
+def stayHomePath():
+    print()
+    print("You decide to stay at home, and prepare for the worst.")
+    time.sleep(2)
+    print()
+    print("Your dad and brother go to the garage to get supplies to board up the house.")
+    time.sleep(2)
+    print()
+    print("In the meantime, you hear screaming outside in the street. It sounds like your neighbor.")
+    time.sleep(2)
+    print()
+    print("Your mom urges you to stay inside, but you want to save your childhood friend.")
+    time.sleep(2)
+    print()
+    response = ""
+    while response != "1" and response != "2":
+        response = input(
+            "Enter 1 to stay inside, 2 to go help your friend: ")
+    checkHomePath(response)
+
+
+def checkHomePath(chosenPath):
+    friendChoice = ""
+    print()
+    if chosenPath == "1":
+        print("The screams continue for several minutes, but they eventually fade away.")
+        time.sleep(2)
         print()
+        print("You look out the window to try to get a view of what happened, but you can't see anything.")
+    if chosenPath == "2":
+        print("Despite your mother's pleas, you sprint outside to try to save your friend.")
+        time.sleep(2)
+        print()
+        print("You see him on his back, keeping the zombie off by holding a bat to it's neck horizontally.")
+        time.sleep(2)
+        print()
+        print("You see a large stick on the ground. Do you run across to get the stick first, or go straight to help?")
+        time.sleep(2)
+        print()
+        while friendChoice != "1" and friendChoice != "2":
+            friendChoice = input(
+                "Enter 1 to get the stick, enter 2 to help immediately: ")
+
+        correctChoice = random.randint(1, 2)
+        if friendChoice == str(correctChoice):
+            if friendChoice == "1":
+                print(
+                    "You grab the stick, and get to your friend just in time to save him.")
+                time.sleep(2)
+                print()
+                print(
+                    "He thanks you profusely, and returns home to see his family again.")
+            else:
+                print(
+                    "You run to your friend, and kick the zombie off of him. He runs back into his house, and you do the same.")
+                time.sleep(2)
+                print()
+                print(
+                    "He didn't have time to thank you, but I'm sure he is very grateful.")
+        if friendChoice != str(correctChoice):
+            print()
+            print("Unexpectedly, a swarm of zombies comes from your blind spot. Your mother screams as she watches on.")
+            time.sleep(2)
+            print()
+            print(
+                "As you're being taken down, you see your friend engulfed by a swarm as well. The End.")
+            playAgain()
+
+            # Run the losing game function
+
+
+def storePath():
+    print()
+    print("You leave the house, and set out for the store")
+    time.sleep(2)
+    print()
+    print("You get to the store, and the parking lot looks empty")
+    time.sleep(2)
+    print()
+    print("The front door is broken down, and you hear a noise inside of the store")
+    time.sleep(2)
+    print()
+    response = ""
+    while response != "1" and response != "2":
+        response = input(
+            "Enter 1 to go to the left side, 2 to go to the right side: ")
+    checkStorePath(response)
+
+
+def checkStorePath(chosenPath):
+    correctPath = random.randint(1, 2)
+    print()
+    if correctPath == 1:
+        print(
+            "Suddenly on the right side of the store, you hear and see a horde of zombies")
+        time.sleep(2)
+        print()
+        print("You scream, and you now have the full attention of the zombies.")
+    if correctPath == 2:
+        print("Suddenly on the left side of the store, you hear and see a horde of zombies")
+        time.sleep(2)
+        print()
+        print("You scream, and you now have the full attention of the zombies.")
+    time.sleep(2)
+    print()
+    if chosenPath == str(correctPath):
+        print("Luckily for you, you chose the right side of the store.")
+        time.sleep(2)
+        print()
+        print("You and your Dad are able to escape, gathering a small amount of food and medicine")
+    else:
+        print(
+            "Unfortunately you chose the wrong side, and are quickly engulfed in the swarm")
+        print()
+        playAgain()
         # Run the losing game function
 
-playAgain = "yes"
-while playAgain == "yes" or playAgain == "y":
+
+def playAgain():
     intro()
     choice = choosePath()
-    checkPath(choice)
-    playAgain = input("Do you want to play again? (yes or y to continue): ")
+    choice2 = chooseDirection(choice)
+
+
+playAgain()
